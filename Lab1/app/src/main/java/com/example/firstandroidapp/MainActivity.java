24ba0a3a176b5572424586dd0049c7e0228956f1
Nicholas Mulrooney (1000041188) <MULRNK1@student.op.ac.nz>
2019-02-21 16:49:54 +1300

    Finished lab 1.2a

--- Lab1/app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ Lab1/app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textRandomString = (TextView)findViewById(R.id.textDisplay);
+        textRandomString.setText(randomString());
+    }
+
+    public String randomString()
+    {
+        Random random = new Random();
+        int index = random.nextInt(4);
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("Poodle");
+        strings.add("Labrador");
+        strings.add("Shar Pei");
+        strings.add("Newfoundland");
+        return strings.get(index);
     }
 }
