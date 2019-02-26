01680ed75ede34751444f149532faddbd7cd33e7
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-02-26 16:27:08 +1300

    Finished Lab 1.2

--- Lab2/app/src/main/java/com/example/febfridays/MainActivity.java
+++ Lab2/app/src/main/java/com/example/febfridays/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.febfridays;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView displayArray =(TextView)findViewById(R.id.baseText);
+        displayArray.setText(showArray(R.array.FebFridays));
+    }
+
+    public String showArray(int id)
+    {
+        Resources resourceResolver = getResources();
+        int numbers[] = resourceResolver.getIntArray(id);
+        String returnString = resourceResolver.getString(R.string.caption_feb);
+        for (int i : numbers)
+        {
+            returnString = returnString.concat(" " + i);
+        }
+        return returnString;
     }
 }
