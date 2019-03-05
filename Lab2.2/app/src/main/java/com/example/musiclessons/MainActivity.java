2c20c25252c87740e936494da777f73039c3fcaa
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-03-05 16:45:59 +1300

    Updated app to use Activities and Intents

--- Lab2.2/app/src/main/java/com/example/musiclessons/MainActivity.java
+++ Lab2.2/app/src/main/java/com/example/musiclessons/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.musiclessons;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,41 +15,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Intent startEnrolment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Populating the dropdown box for the months
-        Spinner monthSpinner = (Spinner)findViewById(R.id.monthSpinner);
-        int layoutID = android.R.layout.simple_spinner_item;
-        Resources resourceResolver = getResources();
-        String months[] = resourceResolver.getStringArray(R.array.months);
-        ArrayAdapter<String> monthAdapter = new ArrayAdapter<String>(this, layoutID, months);
-        monthSpinner.setAdapter(monthAdapter);
+        startEnrolment = new Intent(this, EnrolActivity.class);
 
-        Button enrolButton = (Button) findViewById(R.id.buttonEnrol);
-        ButtonClickHandler handler = new ButtonClickHandler();
-        enrolButton.setOnClickListener(handler);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runEnrolment();
+            }
+        }, 5000);
     }
 
-    public class ButtonClickHandler implements View.OnClickListener
+    private void runEnrolment()
     {
-        @Override
-        public void onClick(View v)
-        {
-            RadioGroup instruments = (RadioGroup)findViewById(R.id.radioButtons);
-            Resources resourceResolver = getResources();
-            String congratsMessage = resourceResolver.getString(R.string.congratulations);
-            RadioButton selectedRBTN = (RadioButton)findViewById(instruments.getCheckedRadioButtonId());
-            congratsMessage = congratsMessage.replace(resourceResolver.getString(R.string.congratulationsI), selectedRBTN.getText());
-
-            Spinner monthSpinner = (Spinner)findViewById(R.id.monthSpinner);
-            congratsMessage = congratsMessage.replace(resourceResolver.getString(R.string.congratulationsM), monthSpinner.getSelectedItem().toString());
-
-            TextView congratsText = (TextView)findViewById(R.id.congratsText);
-
-            congratsText.setText(congratsMessage);
-        }
+        startActivity(startEnrolment);
     }
 }
